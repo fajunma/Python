b40cf08e3847d807010cea3ffe83f3660e3c204e
fajunma <fajunma@users.noreply.github.com>
2019-01-15 18:00:55 +0800

    //add a new ocr py code

--- SQL/firstSQL.py
+++ SQL/firstSQL.py
@@ -5,14 +5,16 @@ import sqlite3
 from random import *
 conn=sqlite3.connect('test.db')
 cur=conn.cursor()
-# try:
-#     cur.execute('CREATE TABLE tableTest(field1 numeric, field2 text)')
-# except:
-#     pass
-# t='abcd'
-# data=zip([random() for x in range(len(t))],t)
-# cur.executemany('INSERT INTO tableTest values(?,?)',data)
+try:
+    cur.execute('CREATE TABLE tableTest(field1 numeric, field2 text)')
+except:
+    pass
+t='abcd'
+data=zip([random() for x in range(len(t))],t)
+cur.executemany('INSERT INTO tableTest values(?,?)',data)
 #cur.execute('DROP TABLE tableTest')
 cur.execute('SELECT * FROM tableTest ORDER BY field2 DESC')
 for rec in cur.fetchall():
     print(rec)
+conn.commit()
+conn.close()
