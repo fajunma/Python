a513e02ee8cc616a4115337bd67022c09c2b6014
fajunma <fajunma@users.noreply.github.com>
2019-01-15 17:34:10 +0800

    //add a new ocr py code

--- DLL/paimai.py
+++ DLL/paimai.py
@@ -6,7 +6,7 @@ import re
 import matplotlib.pyplot as plt
 from datetime import datetime
 import matplotlib.dates as mdates
-
+import sqlite3
 u2018 = 'http://chepai.alltobid.com/contents/22/276.html'
 u2017 = 'http://chepai.alltobid.com/contents/22/103.html'
 def get_paimai_price(url):
@@ -47,13 +47,46 @@ def get_paimai_price(url):
 # print('平均价格是--',sum(price)//len(price),'\nMax--',max(price),'\nLowest--',min(price))
 
 if __name__=="__main__":
-    u2018 = 'http://chepai.alltobid.com/contents/22/276.html'
+    u2018 = 'http://chepai.alltobid.com/contents/22/281.html'
     u2017 = 'http://chepai.alltobid.com/contents/22/103.html'
     info = []
+
+    conn = sqlite3.connect('price.db')
+    cur = conn.cursor()
+    try:
+        cur.execute('''CREATE TABLE PRICE2018
+                       (拍卖时间 CHAR[50]    ,
+                       投放数量 CHAR[50]   ,
+                       最低成交价格 CHAR[50]     ,
+                       平均成交价格 CHAR[50]   ,
+                       投标人数 CHAR[50]  ,
+                       最低成交价格截止时间  CHAR[50]   );''')
+    except:
+        pass
+
+    dt = cur.execute('SELECT * FROM PRICE2018 ')
+    for x in dt:
+        print('database--', x[0])
+
+    # dt=cur.execute('SELECT * FROM PRICE2018 GROUP BY 拍卖时间 HAVING COUNT(*)>1')
+    # for x in dt:
+    #     print('REP--',x)
+
+    # cur.execute("DELETE FROM PRICE2018 WHERE 拍卖时间='2018年5月'")
+    cur.execute('DELETE FROM PRICE2018 WHERE ROWID NOT IN (SELECT MIN(ROWID) FROM PRICE2018 GROUP BY 拍卖时间)')
+
+    dt = cur.execute('SELECT * FROM PRICE2018 ')
+    for x in dt:
+        print('database--', x)
+
+
+    # NAME=cur.execute('SELECT * FROM PRICE2018 WHERE type=’拍卖时间’ ')
+    # print(NAME)
     #get_paimai_price(u2017)
     get_paimai_price(u2018)
     reslt=[]
     seconds=[]
+    avg=[]
     date=[]
     lowest=[]
     for x in info:
@@ -61,12 +94,24 @@ if __name__=="__main__":
             reslt.append(x)
     for x in reslt[1:]:
         print(x)
+        # sqlone = "INSERT INTO PRICE2018 VALUES(%s)" % (','.join(x))
+        # cur.execute(sqlone)
         try:
+            sqlone = "INSERT INTO PRICE2018(拍卖时间,投放数量,最低成交价格,平均成交价格,投标人数,最低成交价格截止时间) VALUES{0}".format(tuple(x))
+            cur.execute(sqlone)
             seconds.append(int(x[4][6:9]))
-            lowest.append(int(x[2]))
+            lowest.append(float(x[2]))
+            avg.append(float(x[3]))
             date.append (datetime.strptime(x[0], '%Y年%m月'))
         except:
             pass
+
+    print(reslt)
+
+    conn.commit()
+    conn.close()
+
+
 
     plt.figure()
     plt.subplot(2,1,1)
@@ -87,3 +132,5 @@ if __name__=="__main__":
     plt.ylabel(r'y =last second')
     plt.grid()
     plt.show()
+
+    # print(sum(avg)/len(avg))
