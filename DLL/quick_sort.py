42440ba8e93af0c2aa591f1210b1915d07d01f7b
fajunma <fajunma@qq.com>
2019-01-18 08:29:01 +0800

    //errectmmine diger

---
+++ DLL/quick_sort.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+
+def f(lst):
+    if len(lst)<=1:
+        return lst
+    else:
+        m=[lst[0]]
+        l=[]
+        r=[]
+        for x in  lst[1:]:
+            if x<=lst[0]:
+                l.append(x)
+            else:
+                r.append(x)
+        return f(l)+m+f(r)
+
+
+a=[2,4,1,0,9,7,6,9,5,1,0,8]
+print(f(a))
+
