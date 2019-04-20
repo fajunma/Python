e00faf35632095e6472c65793bff39aff2a22e9a
fajunma <fajunma@qq.com>
2019-04-21 00:23:26 +0800

    adding leeting code

---
+++ SQL/leetcode.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+_author_ = 'jackie.ma'
+
+
+def str2int(s1):
+    t = 0
+    for i in range(len(s1)):
+        t = t+(ord(s1[i])-ord('0'))*10**(len(s1)-i-1)
+    return t
+
+
+if __name__ == "__main__":
+    a = '1234987'
+    print(str2int(a))
+
+    print(sum([10**x[0]*(ord(x[1])-48) for x in enumerate(a[::-1])]))
+
+
